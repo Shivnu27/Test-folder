9c98891f949cece5b2f343ec4376ca0e45e69942
Shivnu27 <manirathi2710.com>
2026-06-15 14:48:07 +0530

    first commit

---
+++ test.ts
@@ -0,0 +1,23 @@
+ let firstName: string = "Manikandan.M"; 
+ let desiganation: string = "SENIOR OPERATIONS ASSOCIATE";
+ let email: string = "manirathi2710@gmail.com";
+ let age: number = 26;
+ let objective: string = "To obtain a challenging role where I can apply my expertise in operations management, process optimization, and team leadership to drive efficiency and contribute to the organization’s overall success" ;
+ let workExperience: string = "Jan 2023 - Till date Opendoor (Services Labs Software Private Limited)";
+ let keyAchievement: string = "Selected to join the Operational Excellence team in recognition of consistent high performance, gained in-depth knowledge of the Opendoor process end-to-end. Honored with the Most Valuable Performer award multiple times for exceptional contribution and results. Received personal commendation from the Head of Operations for dedication and performance excellence. Consistently ranked among the top-performing team members, maintaining highquality output and reliability.";
+ let education: string = "UG - ASAN MEMORIAL COLLEGE OF ARTS AND SCIENCE 2018 - 2020 PG - MADRAS UNIVERSITY 2022 - 2023";
+
+ console .log(`Hi i am in ${firstName} I have ${workExperience}`);
+
+
+//  console .log(`Name: ${firstName}`); 
+//  console .log(`desiganation: ${desiganation}`);
+//  console .log(`email: ${email}`);
+//  console .log(`Age: ${age}`);
+//  console .log(`objective: ${objective}`);
+//  console .log(`workexperience: ${workExperience}`)
+//  console .log(`keyachievement: ${keyAchievement}`);
+//  console .log(`education = ${education}`);
+ 
+
+  
