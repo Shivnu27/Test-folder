b267fd5b913a645b3296c9fdbaaccc00b6b4aab4
Shivnu27 <manirathi2710.com>
2026-06-16 13:49:44 +0530

    test

---
+++ src/dataType.ts/var_stl.ts
@@ -0,0 +1,5 @@
+const text: string = " Welcome Tp TypeScript Automation Testing ";
+const email: string = "shivnu@gmail.com";
+const empID: string = "EMP1234";
+console.log("Original String");
+console.log(text);
